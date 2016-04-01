791018fb19a561bbdd627b364ce54d638226b5c4
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2016-04-01 14:43:39 +0700

    Create phantomjsInstaller.sh

---
+++ phantomjsInstaller.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev -y
+cd ~/Downloads
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+sudo tar xvjf phantomjs-2.1.1-linux-x86_64
+sudo mv phantomjs-2.1.1-linux-x86_64 /usr/local/share
+sudo ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
+phantomjs --version
